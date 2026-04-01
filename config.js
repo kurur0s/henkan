92e6c303620aa420192a457a17f8584446c9bcb2
Angel <140189442+kurur0s@users.noreply.github.com>
2026-03-31 21:04:53 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// ⚠️  Add this file to your .gitignore — never commit your API key!
+const CONFIG = {
+  GENIUS_ACCESS_TOKEN: "M7WotH38fz1JgfX3SgfcwajDqoaWXlwxEVHk2_3ffsnitqiEKCftGRLiWSal99QP",
+};
