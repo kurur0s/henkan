881e8e930a9f17cbf4b49846284eb655927da3f9
Angel <140189442+kurur0s@users.noreply.github.com>
2026-03-31 21:59:56 -0300

    updated token
    Updated the GENIUS_ACCESS_TOKEN in config.js.

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // ⚠️  Add this file to your .gitignore — never commit your API key!
 const CONFIG = {
-  GENIUS_ACCESS_TOKEN: "M7WotH38fz1JgfX3SgfcwajDqoaWXlwxEVHk2_3ffsnitqiEKCftGRLiWSal99QP",
+  GENIUS_ACCESS_TOKEN: "RnSAi3ttRvwMb7jmViVyTnAW9vG3S62BLdzeacR7k6hYAu-uQQAHBO9Mo3H7F-fb",
 };
